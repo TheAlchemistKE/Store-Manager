7cb40cb58d4c310ec6838ec9cfcb8f2d945eaa1f
KelynPNjeri <kelynpaul20.kp@gmail.com>
2018-10-19 01:07:06 +0300

    :new:[Feature #161330910]CreateGet Specific Sales Record

--- app/api/v1/views/sales_views.py
+++ app/api/v1/views/sales_views.py
@@ -15,3 +15,29 @@ class SalesList(Resource, SalesOps):
             "Sale Records": self.sales_obj.retrieve_all_items()
         }
         return make_response(jsonify(resp), 200)
+
+    def post(self):
+        data = request.get_json()
+        sales_by = data['Sold By']
+        quantity_sold = data['Quantity Sold']
+        sales_date = data['Date Created']
+        unit_price = data['Price per unit']
+
+        resp = {
+            "Message": "Created.",
+            "Status": "OK",
+            "Sales Records": self.sales_obj.save_sales_record(sales_by, quantity_sold, sales_date, unit_price)
+        }
+        return make_response(jsonify(resp), 201)
+
+
+class SingleSaleRecords(Resource):
+    def __init__(self):
+        self.records_obj = SalesOps()
+
+    def get(self, sale_id):
+        resp = {
+            "Message": "Successful.",
+            "Product": self.records_obj.show_one(sale_id)
+        }
+        return make_response(jsonify(resp), 200)
