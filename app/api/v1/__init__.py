f5759fda1a21a0bcc9a9f98701be6328e3b97fa1
KelynPNjeri <kelynpaul20.kp@gmail.com>
2018-10-18 19:51:28 +0300

    :new:[Feature #161265803]Create Admin can get all Products

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 # Local Imports
-from .views.product_views import ProductList, SingleProduct
+from .views.product_views import ProductList
 
 
 version1 = Blueprint('api', __name__, url_prefix='/api/v1')
