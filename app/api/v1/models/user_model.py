3b0258ef30317cfb107cbe7f1bc48e0ba0bb9d59
KelynPNjeri <kelynpaul20.kp@gmail.com>
2018-10-20 09:19:01 +0300

    [Chore #161359555]Implement JWT authentication

--- app/api/v1/models/user_model.py
+++ app/api/v1/models/user_model.py
@@ -1,8 +1,48 @@
+from flask_restful import reqparse
+from flask import jsonify, make_response
+
+
+
+
+parser = reqparse.RequestParser()
+parser.add_argument("username", type=str,
+                    help="Username cannot be left blank.")
+parser.add_argument("password", type=str,
+                    help="Password cannot be left blank.")
+parser.add_argument("role", type=str, help="Role cannot be left blank.")
 
 users = []
 
-user_payload = {
-    
-}
+
 def authenticate():
     pass
+
+
+class Users():
+    def __init__(self, name, username, email, password):
+        self.name = name
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def signup(self):
+        user_data = parser.parse_args()
+        username = user_data["username"]
+        password = user_data["password"]
+
+        user_payload = {
+            "Id": len(users)+1,
+            "Username": username,
+            "Password": password
+        }
+        users.append(user_payload)
+
+    def login(self, username, password):
+        for user in users:
+            if user["Username"] == username:
+                return user
+            else:
+                return make_response(jsonify({"message": "User not found."}), 404)
+
+    def logout(self):
+        pass
