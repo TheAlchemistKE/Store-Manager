b0af31beba894b10b973bd56201a9a95acccd1b0
KelynPNjeri <kelynpaul20.kp@gmail.com>
2018-10-25 11:44:11 +0300

    [Chore #161345063]Validating JSON input

--- app/api/v1/utils/validator.py
+++ app/api/v1/utils/validator.py
@@ -1,26 +1,4 @@
 from flask import jsonify, make_response
 
-
-# Checking if the fields are empty.
-def product_validator(data):
-    if bool(data["Product Name"]) != True:
-        return make_response(jsonify({"message": "Product Name cannot be empty."}), 400)
-    elif type(data["Product Name"]) == int:
-        return make_response(jsonify({"message": "Product Name should be a string."}), 400)
-
-    elif bool(data["Product Category"]) != True:
-        return make_response(jsonify({"message": "Product Category cannot be empty."}), 400)
-
-    else:
-        return "OK"
-
-
-def sales_validator(data):
-    if bool(data['Sold By']) != True:
-        return make_response(jsonify({"message": "Product Name cannot be empty."}), 400)
-
-    elif bool(data['Date Created']) != True:
-        return make_response(jsonify({"message": "Product Category cannot be empty."}), 400)
-
-    else:
-        return "OK"
+def validator(**args):
+    for ()
